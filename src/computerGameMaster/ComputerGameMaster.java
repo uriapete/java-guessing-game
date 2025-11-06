c1a0ed5b886da54bbbc34702f35f93506a398051
uriapete <uriarteangelo@gmail.com>
2025-11-05 17:08:50 -0800

    ComGameMaster.java - random number generator and stored as instance field

--- src/computerGameMaster/ComputerGameMaster.java
+++ src/computerGameMaster/ComputerGameMaster.java
@@ -7,12 +7,28 @@ package computerGameMaster;
  * 
  */
 public class ComputerGameMaster {
+	private int number;
+	private static int[] bounds = {1,100};
+	
+	public int getNumber(){
+		return number;
+	}
+
+	public void generateRandomNumber(){
+		// source: https://www.baeldung.com/java-generating-random-numbers
+		number = (int) ((Math.random() * (bounds[1] - bounds[0])) + bounds[0]);
+	}
 
 	/**
 	 * @param args
+	 * FOR TESTING ONLY
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ComputerGameMaster gm = new ComputerGameMaster();
+		gm.generateRandomNumber();
+	
+		System.out.println(gm.getNumber());
 
 	}
 
