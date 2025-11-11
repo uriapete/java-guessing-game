8f2b06ce074258d5deff1a43edfaf75055f2c3cc
uriapete <uriarteangelo@gmail.com>
2025-11-10 18:41:37 -0800

    TermInt.j - new field menuOptions

--- src/terminalAppInterface/TerminalInterface.java
+++ src/terminalAppInterface/TerminalInterface.java
@@ -7,6 +7,14 @@ package terminalAppInterface;
  * 
  */
 public class TerminalInterface {
+	/*
+	 * String options for the terminal menu.
+	 */
+	private static String[] menuOptions = {
+		"I pick a number",
+		"You pick a number"
+	};
+	
 	/**
 	 * @param args
 	 */
