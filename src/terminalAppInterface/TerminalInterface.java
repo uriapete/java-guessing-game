ade793d22be16984676fe1305c1a709f6a5bf80b
uriapete <uriarteangelo@gmail.com>
2025-11-10 18:35:52 -0800

    new class terminalAppInterface/TerminalInterface

---
+++ src/terminalAppInterface/TerminalInterface.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package terminalAppInterface;
+
+/**
+ * 
+ */
+public class TerminalInterface {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
